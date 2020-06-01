97547ad8006b75f412b24bed586552682b2a0462
yang.bo <yangb@kh-super.com>
2020-06-01 14:28:33 +0800

    feat(3): 增加java并发编程 安全性之原子性操作解决办法分析

---
+++ src/main/java/com/uncle/controller/thread/two/Increment.java
@@ -0,0 +1,57 @@
+package com.uncle.controller.thread.two;
+
+/**
+ * @author 杨戬
+ * @className Increment
+ * @email yangb@chaosource.com
+ * @date 20-6-1 11:35
+ */
+public class Increment {
+    private int i;
+
+    public synchronized void increase() {
+        i++;
+    }
+
+    public int getI() {
+        return i;
+    }
+
+    public static void test(int threadNum, int loopTimes) {
+        Increment increment = new Increment();
+
+        Thread[] threads = new Thread[threadNum];
+
+        for (int i = 0; i < threads.length; i++) {
+            Thread t = new Thread(new Runnable() {
+                @Override
+                public void run() {
+                    for (int i = 0; i < loopTimes; i++) {
+                        increment.increase();
+                    }
+                }
+            });
+            threads[i] = t;
+            t.start();
+        }
+
+        for (Thread t : threads) {  //main线程等待其他线程都执行完成
+            try {
+                t.join();
+            } catch (InterruptedException e) {
+                throw new RuntimeException(e);
+            }
+        }
+
+        System.out.println(threadNum + "个线程，循环" + loopTimes + "次结果：" + increment.getI());
+    }
+
+    public static void main(String[] args) {
+        test(20, 1);
+        test(20, 10);
+        test(20, 100);
+        test(20, 1000);
+        test(20, 10000);
+        test(20, 100000);
+    }
+}
